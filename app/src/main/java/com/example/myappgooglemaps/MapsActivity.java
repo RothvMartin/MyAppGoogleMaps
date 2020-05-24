030a28aa622cffc28cb5d2e6092eeee8a39878a2
RothvMartin <62974036+RothvMartin@users.noreply.github.com>
2020-05-24 14:22:16 +1200

    Added new Main Activity

--- app/src/main/java/com/example/myappgooglemaps/MapsActivity.java
+++ app/src/main/java/com/example/myappgooglemaps/MapsActivity.java
@@ -1,19 +1,46 @@
 package com.example.myappgooglemaps;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.w3c.dom.Text;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    Button btnLoadCoordinates;
+    TextView textView;
+    FusedLocationProviderClient client;
+    Boolean clientPermissions = false;
+//    Location clientLocation;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +50,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        btnLoadCoordinates = findViewById(R.id.btnMap);
+        textView = findViewById(R.id.textView);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            clientPermissions = true;
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 901);
+        }
+
+
+
+
+
+
+        SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
+
+
+
+
+
     }
 
 
@@ -39,9 +92,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        btnLoadCoordinates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //This code is to enable location movement via the extended emulator interface
+                //i.e. emulating different locations
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                try{
+                    if(clientPermissions){
+                        loadCoordinates();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Permission was denied.", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                } catch (SecurityException ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+/*        LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
     }
+
+
+    public void loadCoordinates(){
+        Task<Location> location = client.getLastLocation();
+
+
+        //Consider removing context from params (this)
+        location.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if(task.isSuccessful()){
+                    String result = Double.toString(task.getResult().getLatitude()) + "\n" +
+                            Double.toString(task.getResult().getLongitude());
+                    textView.setText(result);
+                }
+
+            }
+        });
+    }
+
 }
